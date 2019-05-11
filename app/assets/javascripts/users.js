7066b91f1976e4b99b2dcb07ba8e59c6dea31b90
github username <alekvuka@gmail.com>
2019-05-10 23:23:30 -0400

    add gems and scripts necessary for jquery to load

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,15 +2,13 @@
 //function to get the number or routines a user has
 
 
+
 function userInfo() {
-   let id = $('#user_id').value
+
+   let id = $('#user_id')[0].value
    $.getJSON("/users/" + id, function(result){
      debugger
    })
-
-
-
-
 
 }
 
