75694b0910e487f95d3549b3f6bfe379b1c492a1
github username <alekvuka@gmail.com>
2019-05-10 22:54:33 -0400

    add jquery-rails gem to app

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,44 @@
+
+//function to get the number or routines a user has
+
+
+function userInfo() {
+   let id = $('#user_id').value
+   $.getJSON("/users/" + id, function(result){
+     debugger
+   })
+
+
+
+
+
+}
+
+function numberOfRoutines() {
+
+}
+
+//function to get the number of tasks a user has
+function numberOfTasks() {
+
+}
+
+//function for current routine section
+function currentRoutines(){
+
+}
+
+//function for upcoming routines
+function upcomingRoutines() {
+
+}
+
+
+
+
+$(document).ready(function() {
+  userInfo()
+  //currentRoutines()
+  //upcomingRoutines()
+  //attachListeners()
+});
