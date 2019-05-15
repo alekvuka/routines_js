9aa4e0bd3ec10b1b28372bbcac77d40f5b90e7bb
github username <alekvuka@gmail.com>
2019-05-15 02:40:34 -0400

    delete create_with_google method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,19 +10,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  def create_with_google
-    user = User.find_by(email: auth[:info][:email], name: auth[:info][:name])
-    if user
-      user.uid = auth[:uid]
-      user.save
-      session[:user_id] = user.id
-      redirect_to user_path(current_user)
-    else
-      flash[:alert] = "Google authentication failed"
-      render 'static/index'
-    end
-  end
-
   def destroy
     session.clear
     redirect_to root_path
